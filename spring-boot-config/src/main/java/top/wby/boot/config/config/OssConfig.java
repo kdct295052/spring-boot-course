300a1e2948d568b5264f37ad125287b9fe3a94b7
王博钰 <15668186+kdct295052@user.noreply.gitee.com>
2025-09-13 13:18:01 +0800

    feat:整合阿里云OSS上传

--- spring-boot-config/src/main/java/top/wby/boot/config/config/OssConfig.java
+++ spring-boot-config/src/main/java/top/wby/boot/config/config/OssConfig.java
@@ -3,10 +3,12 @@ package top.wby.boot.config.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "aliyun-oss")
+@PropertySource("classpath:aliyun-oss.properties")
 public class OssConfig {
     private String endpoint;
     private String bucketName;
