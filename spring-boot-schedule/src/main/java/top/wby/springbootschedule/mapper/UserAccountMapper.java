c4698e6138601c74f19e7f32ad3d64dc5b85638e
王博钰 <15668186+kdct295052@user.noreply.gitee.com>
2025-10-11 13:19:55 +0800

    feat:定时任务

---
+++ spring-boot-schedule/src/main/java/top/wby/springbootschedule/mapper/UserAccountMapper.java
@@ -0,0 +1,10 @@
+package top.wby.springbootschedule.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.wby.springbootschedule.entity.UserAccount;
+
+@Mapper
+public interface UserAccountMapper extends BaseMapper<UserAccount> {
+
+}
